c7153c40a08b88d8fd5f08f4516a0e96fcfb351d
johnnyringo <fanslerorama@gmail.com>
2023-01-31 18:24:56 -0800

    main

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -169,8 +169,8 @@ export default function LoginPage() {
                 }}
               >
                 Sign up
-              </Link>*/}
-            </div>
+              </Link>
+            </div>*/}
           </div>
         </Form>
       </div>
